538f50e493bb0af8edb078a50b1726d01c72e541
Abhinav <abhinav.chanda99@gmail.com>
2019-05-18 16:31:10 -0400

    Parsing Prescription

--- my-app/server.js
+++ my-app/server.js
@@ -8,21 +8,28 @@ const express = require('express');
 let request = require('request');
 let http = require('http');
 let app = express();
-let apikey = "AIzaSyAKDu8ti6WtofsN6S5oH8GByrZL0zasCVs";
+let apiKey = "AIzaSyBuBIYaXJxNOzqeIgXNkJZu0e9GZmQZQeA";
 
 const cors = require('cors')
 
 var corsOptions = {
   origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200 
+  optionsSuccessStatus: 200
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(bodyParser({
+app.use(bodyParser.json({
   limit: '10mb'
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// app.use(bodyParser({
+//   limit: '10mb'
+// }));
+
 
 
 
@@ -39,57 +46,87 @@ app.get('/', (req, res) => {
 
 app.post('/submitPrescription', function (req, res) { //req from angular front end
   //console.log(req.body.prescriptionImg);
-  function apiCall(){
-      return new Promise((resolve, reject) => {
-        let img = req.body.prescriptionImg;
-        console.log('https://vision.googleapis.com/v1/images:annotate?key=' + apikey);
-        let options = { method: 'POST',
+  function apiCall() {
+    return new Promise((resolve, reject) => {
+      let img = req.body.prescriptionImg;
+      console.log('https://vision.googleapis.com/v1/images:annotate?key=' + apiKey);
+
+      let options = {
+        method: 'POST',
         url: 'https://vision.googleapis.com/v1/images:annotate',
-        qs: { key: 'AIzaSyAKDu8ti6WtofsN6S5oH8GByrZL0zasCVs' },
-        body: '{  "requests": [    {      "image": {        "content": "'+ img +'"     },      "features": [        {          "type": "TEXT_DETECTION"        }      ]    }  ]}' };
-      
-        request(options, function (error, response, body) {         
-            //console.log((JSON.parse(body).responses[0])['textAnnotations'][0]['description']);
-            resolve(body);
-        });
+        qs: {
+          key: apiKey
+        },
+        headers: {
+          'cache-control': 'no-cache',
+          'Content-Type': 'application/json'
+        },
+        body: {
+          requests: [{
+            image: {
+              content: '' + img
+            },
+            features: [{
+              type: 'DOCUMENT_TEXT_DETECTION'
+            }]
+          }]
+        },
+        json: true
+      };
+
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log("POST REQUEST OCR SUCCESSFUL:");
+        resolve(body['responses'][0]['textAnnotations'][0]['description']);
       });
-  }
 
-  function parse(){
-    return new Promise((resolve, reject) => {
-        apiCall().then((data) => {
-            console.log(JSON.stringify((JSON.parse(data).responses[0])['textAnnotations'][0]['description']));
-
-            resolve(JSON.parse(data).responses[0]);
-        });
-      });
-  }
-
-  function stringify(){
-    return new Promise((resolve, reject) => {
-        parse().then((data) => {
-            console.log(JSON.stringify(data['textAnnotations'][0]['description']));
-
-            resolve(JSON.stringify(data['textAnnotations'][0]['description']));
-        });
-      });
-  }
-
-  function parsePres(){
-    let result = {};
-    return new Promise((resolve, reject) => {
-        stringify().then((data) => {
-            resolve(data.split('\n'));
-        });
     });
-}
-
-  return new Promise ((resolve, reject) => {
-    parsePres().then((data) => {
-        console.log(data);
-        res.send(data);
-    });
+  }
+  apiCall().then((data)=>{
+    console.log(data);
+    console.log(typeof data);
+    let res = data.split("\n");
+    for(let i = 0; i < res.length; i++){
+      res[i] = res[i].split(": ");
+    }
+    console.log("PARSED", res);
   });
+
+  // function parse() {
+  //   return new Promise((resolve, reject) => {
+  //     apiCall().then((data) => {
+  //       console.log(JSON.stringify((JSON.parse(data).responses[0])['textAnnotations'][0]['description']));
+
+  //       resolve(JSON.parse(data).responses[0]);
+  //     });
+  //   });
+  // }
+
+  // function stringify() {
+  //   return new Promise((resolve, reject) => {
+  //     parse().then((data) => {
+  //       console.log(JSON.stringify(data['textAnnotations'][0]['description']));
+
+  //       resolve(JSON.stringify(data['textAnnotations'][0]['description']));
+  //     });
+  //   });
+  // }
+
+  // function parsePres() {
+  //   let result = {};
+  //   return new Promise((resolve, reject) => {
+  //     stringify().then((data) => {
+  //       resolve(data.split('\n'));
+  //     });
+  //   });
+  // }
+
+  // return new Promise((resolve, reject) => {
+  //   parsePres().then((data) => {
+  //     console.log(data);
+  //     res.send(data);
+  //   });
+  // });
 });
 
 // .forEach((data) => {
