06e86b8ff64fa69421000b0ef49c604e8c240dcb
Pronoy Chaudhuri <pronoyy@gmail.com>
2019-05-18 20:23:56 -0400

    add

--- my-app/src/app/confirmation/confirmation.component.ts
+++ my-app/src/app/confirmation/confirmation.component.ts
@@ -11,11 +11,11 @@ export class ConfirmationComponent implements OnInit {
   //Change this up if you wanna
   reqBody = {
     doctorName: 'Dev Patel',
-    // doctorAddress: '8 Adelaide St W Toronto',
+    doctorAddress: '8 Adelaide St W Toronto',
     patientName: 'Mindy Sharpe',
-    // patientAddress: '12 Adelaide St Toronto',
-    // patientAge: '30',
-    // patientSex: 'F',
+    patientAddress: '12 Adelaide St Toronto',
+    patientAge: '30',
+    patientSex: 'F',
     datePrescribed: '16-02-2019',
     drugName: 'Amphetamins',
     drugAmount: '500mg',
